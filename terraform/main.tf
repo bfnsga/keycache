15805307eaadb5077dee31d84d1d69bc477a1f2b
Ben Field <bfnsga@gmail.com>
2023-01-06 17:57:08 -0500

    Added AppRunner module

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,11 @@ module "codebuild" {
   aws_region = var.aws_region
   github_credentials = var.github_credentials
 }
+
+module "apprunner" {
+  source = "./modules/apprunner"
+
+  repository_name = module.ecr.repository_name
+  aws_account_id = var.aws_account_id
+  aws_region = var.aws_region
+}
---
+++ terraform/modules/apprunner/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_account_id" {
+    type = string
+}
+
+variable "aws_region" {
+    type = string
+}
+
+variable "repository_name" {
+    type = string
+}
