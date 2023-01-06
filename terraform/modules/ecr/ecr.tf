209a145dca88c19cb09bd3422988097a7fbb2629
Ben Field <bfnsga@gmail.com>
2023-01-06 15:58:29 -0500

    Added ECR repo lifecycle policy

--- terraform/modules/ecr/ecr.tf
+++ terraform/modules/ecr/ecr.tf
@@ -1,8 +1,34 @@
-resource "aws_ecr_repository" "ecr_repository" {
+#################################
+## ECR Private Repository
+#################################
+resource "aws_ecr_repository" "production" {
   name                 = "production"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+resource "aws_ecr_lifecycle_policy" "policy" {
+  repository = aws_ecr_repository.production.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
 }
