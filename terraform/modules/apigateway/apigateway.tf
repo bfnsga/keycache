8635fd07eaf04123d966c5f239f8f67a73a7e62f
Ben Field <bfnsga@gmail.com>
2023-01-08 14:48:34 -0500

    API Gateway Terraform testing

--- terraform/modules/apigateway/apigateway.tf
+++ terraform/modules/apigateway/apigateway.tf
@@ -1,3 +1,20 @@
 resource "aws_api_gateway_rest_api" "example" {
   name = "example"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_resource" "example" {
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "example"
+  rest_api_id = aws_api_gateway_rest_api.example.id
+}
+
+resource "aws_api_gateway_method" "example" {
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
 }
