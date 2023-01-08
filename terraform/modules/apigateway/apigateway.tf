16890ee47c14e1e971cc0d4f41025ddc67966f28
Ben Field <bfnsga@gmail.com>
2023-01-08 15:12:11 -0500

    API Gateway Terraform testing

--- terraform/modules/apigateway/apigateway.tf
+++ terraform/modules/apigateway/apigateway.tf
@@ -1,3 +1,59 @@
+#########################
+## API Gateway
+#########################
+
+## IAM Role
+#########################
+resource "aws_iam_role" "apigateway_role" {
+  name        = "APIGatewayBaseRole"
+  description = "Allows API Gateway to access AWS resources."
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : "apigateway.amazonaws.com"
+        },
+        "Effect" : "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "apigateway_policy" {
+  name        = "APIGatewayBasePolicy"
+  description = "Allows API Gateway to access AWS resources."
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Resource" : "*",
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams",
+          "logs:PutLogEvents",
+          "logs:GetLogEvents",
+          "logs:FilterLogEvents",
+          "dynamodb:PutItem"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "apigateway_role_policy" {
+  role       = aws_iam_role.apigateway_role.name
+  policy_arn = aws_iam_policy.apigateway_policy.arn
+}
+
+## API Gateway
+#########################
 resource "aws_api_gateway_rest_api" "example" {
   name = "example"
 
@@ -24,8 +80,11 @@ resource "aws_api_gateway_integration" "example" {
   resource_id = aws_api_gateway_resource.example.id
   http_method = aws_api_gateway_method.example.http_method
   type        = "AWS"
+  credentials = aws_iam_role.apigateway_role.arn
   integration_http_method = "POST"
   uri         = "arn:aws:apigateway:us-east-2:dynamodb:action/PutItem"
+  passthrough_behavior    = "NEVER"
+
   request_templates = {
     "application/json" = <<EOF
 {
