16d7873abde29cbb1e5a0b9eff8ff21d58df5cab
Ben Field <bfnsga@gmail.com>
2023-01-08 15:08:16 -0500

    API Gateway Terraform testing

--- terraform/modules/apigateway/apigateway.tf
+++ terraform/modules/apigateway/apigateway.tf
@@ -18,3 +18,27 @@ resource "aws_api_gateway_method" "example" {
   resource_id   = aws_api_gateway_resource.example.id
   rest_api_id   = aws_api_gateway_rest_api.example.id
 }
+
+resource "aws_api_gateway_integration" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example.id
+  http_method = aws_api_gateway_method.example.http_method
+  type        = "AWS"
+  integration_http_method = "POST"
+  uri         = "arn:aws:apigateway:us-east-2:dynamodb:action/PutItem"
+  request_templates = {
+    "application/json" = <<EOF
+{
+  "TableName": "example",
+  "Item": {
+    "id": {
+      "S": "${uuid()}"
+    },
+    "name": {
+      "S": "testing"
+    }
+  }
+}
+EOF
+  }
+}
